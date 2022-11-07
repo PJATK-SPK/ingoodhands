7e741e294947e7964b4c6ba70994f8d138d65ab5
marcel <marcel.pankanin@ramp.network>
2022-11-07 20:53:17 +0100

    feat(infra): add firebase to project

--- infra/scripts/push_images.sh
+++ infra/scripts/push_images.sh
@@ -1,10 +1,6 @@
 image_to_build=$1
 google_project="${2:-indagoodhandsdev}"
 
-function go_to_root {
-  cd ../..
-}
-
 function login {
   gcloud auth configure-docker europe-docker.pkg.dev
 }
@@ -15,7 +11,6 @@ function build_tag {
     echo "Please provide image name to build"
     exit 1
   fi
-  go_to_root
   docker-compose -f docker-compose.cloud.yml build "$image_to_build"
   docker image tag "$image_to_build" europe-docker.pkg.dev/"$google_project"/default-docker/"$image_to_build":latest
   docker push europe-docker.pkg.dev/"$google_project"/default-docker/"$image_to_build":latest
