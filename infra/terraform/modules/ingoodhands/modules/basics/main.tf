84697f22faa21a65e0f807f12ba052dae30e41d3
marcel <marcel.pankanin@ramp.network>
2022-11-10 20:22:06 +0100

    feat(infra): pipeline first version

--- infra/terraform/modules/ingoodhands/modules/basics/main.tf
+++ infra/terraform/modules/ingoodhands/modules/basics/main.tf
@@ -12,3 +12,14 @@ resource "google_artifact_registry_repository" "artifact_registry_docker_repo" {
   format        = "DOCKER"
   repository_id = "default-docker"
 }
+
+resource "google_service_account" "cicd_service_account" {
+  account_id   = "cicd-service-account"
+  display_name = "CICD Service Account"
+}
+
+resource "google_project_iam_member" "cicd_service_account_iam" {
+  project = var.google-project
+  member  = format("serviceAccount:%s", google_service_account.cicd_service_account.email)
+  role    = "roles/artifactregistry.writer"
+}
