789b5e358d9557b6d30243b8688826c5e8c9b87b
h4570 <sandro.sobczynski@gmail.com>
2022-11-20 19:03:58 +0100

    config fix

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   url: `https://indagoodhandsdev.web.app`,
   api: `https://api.indagoodhandsdev.web.app`,
   auth: {
-    authority: 'https://ingoodhands.eu.auth0.com/',
+    authority: 'https://ingoodhands.eu.auth0.com',
     audience: 'ingoodhands',
     clientId: 'KvCQmpdmay63JC5N5ObiK4DuKzVBys7L',
     scopes: 'openid profile offline_access email api',
