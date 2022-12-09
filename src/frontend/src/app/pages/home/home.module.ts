08e5808dc81735909aac31614ef622fb847eb6ec
h4570 <sandro.sobczynski@gmail.com>
2022-12-09 21:38:35 +0100

    add basic layout

--- src/frontend/src/app/pages/home/home.module.ts
+++ src/frontend/src/app/pages/home/home.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { StyleClassModule } from 'primeng/styleclass';
+import { TabViewModule } from 'primeng/tabview';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DinnerComponent } from './dinner/dinner.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    DinnerComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    StyleClassModule,
+    TabViewModule,
   ],
   exports: [
     HomeComponent
--- src/frontend/src/app/pages/secure/secure.module.ts
+++ src/frontend/src/app/pages/secure/secure.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SecureComponent } from './secure.component';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from 'src/app/layout/layout.module';
+import { LayoutModule } from './layout/layout.module';
 import { RouterModule } from '@angular/router';
 import { AuthCheckModule } from './auth-check/auth-check.module';
 import { SecureRoutingModule } from './secure.routing.module';
