6db71a6b01c2da0f5876be6e9d3429d6a96d67a1
h4570 <sandro.sobczynski@gmail.com>
2022-12-12 23:15:31 +0100

    add landing pages

--- src/frontend/src/app/pages/home/home.component.ts
+++ src/frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  public readonly url: string = environment.url;
 }
--- src/frontend/src/app/pages/home/home.module.ts
+++ src/frontend/src/app/pages/home/home.module.ts
@@ -4,18 +4,28 @@ import { HomeComponent } from './home.component';
 import { StyleClassModule } from 'primeng/styleclass';
 import { TabViewModule } from 'primeng/tabview';
 import { NavbarComponent } from './navbar/navbar.component';
-import { DinnerComponent } from './dinner/dinner.component';
+import { DonateComponent } from './donate/donate.component';
+import { RequestHelpComponent } from './request-help/request-help.component';
+import { WorkComponent } from './work/work.component';
+import { ButtonModule } from 'primeng/button';
+import { SplashImgComponent } from './splash-img/splash-img.component';
+import { StepsModule } from 'primeng/steps';
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavbarComponent,
-    DinnerComponent,
+    SplashImgComponent,
+    DonateComponent,
+    RequestHelpComponent,
+    WorkComponent,
   ],
   imports: [
     CommonModule,
+    ButtonModule,
     StyleClassModule,
     TabViewModule,
+    StepsModule,
   ],
   exports: [
     HomeComponent
