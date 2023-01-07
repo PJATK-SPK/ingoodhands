afebc494fcfaa9ce97ef4e803fd089304ff90618
marcel <marcel.pankanin@ramp.network>
2023-01-07 14:52:59 +0100

    feat(infra): add tests in ci

--- infra/scripts/push_images.sh
+++ infra/scripts/push_images.sh
@@ -1,5 +1,6 @@
 image_to_build=$1
-google_project="${2:-indagoodhandsdev}"
+google_project="indagoodhandsdev"
+image_tag="${2:-latest}"
 
 function login {
   gcloud auth configure-docker europe-docker.pkg.dev
@@ -11,9 +12,9 @@ function build_tag {
     echo "Please provide image name to build"
     exit 1
   fi
-  docker-compose -f docker-compose.cloud.yml build "$image_to_build"
-  docker image tag "$image_to_build" europe-docker.pkg.dev/"$google_project"/default-docker/"$image_to_build":latest
-  docker push europe-docker.pkg.dev/"$google_project"/default-docker/"$image_to_build":latest
+  docker-compose -f docker-compose.ci.yml build "$image_to_build"
+  docker image tag "$image_to_build" europe-docker.pkg.dev/"$google_project"/default-docker/"$image_to_build":"$image_tag"
+  docker push europe-docker.pkg.dev/"$google_project"/default-docker/"$image_to_build":"$image_tag"
 
 }
 
