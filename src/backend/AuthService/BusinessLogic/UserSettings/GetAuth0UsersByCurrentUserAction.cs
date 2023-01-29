5960fed07a2e1b497ec7013be16277a656b0b888
s21166-pj <s21166@pjwstk.edu.pl>
2023-01-29 14:42:56 +0100

    Added logger , fixed file name, added validation

--- src/backend/AuthService/BusinessLogic/UserSettings/GetAuth0UsersByCurrentUserAction.cs
+++ src/backend/AuthService/BusinessLogic/UserSettings/GetAuth0UsersByCurrentUserAction.cs
@@ -1,32 +1,40 @@
 ﻿using AuthService.BusinessLogic.Exceptions;
 using AuthService.BusinessLogic.PostLogin;
 using Core.Auth0;
-using Core.Database;
+using Core.Database.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace AuthService.BusinessLogic.UserSettings
 {
-    public class UserSettingsAction
+    public class GetAuth0UsersByCurrentUserAction
     {
         private readonly ICurrentUserService _currentUserService;
-        private readonly UserSettingsService _userSettingsService;
+        private readonly GetAuth0UsersByCurrentUserService _userSettingsService;
         private readonly UserDataValidationService _userDataValidationService;
+        private readonly ILogger<GetAuth0UsersByCurrentUserAction> _logger;
 
-        public UserSettingsAction(ICurrentUserService currentUserService, UserSettingsService userSettingsService, UserDataValidationService userDataValidationService)
+        public GetAuth0UsersByCurrentUserAction(
+            ICurrentUserService currentUserService,
+            GetAuth0UsersByCurrentUserService userSettingsService,
+            UserDataValidationService userDataValidationService,
+            ILogger<GetAuth0UsersByCurrentUserAction> logger)
         {
             _currentUserService = currentUserService;
             _userSettingsService = userSettingsService;
             _userDataValidationService = userDataValidationService;
+            _logger = logger;
         }
 
-        public async Task<ActionResult> Execute()
+        public async Task<OkObjectResult> Execute()
         {
             var auth0UserInfo = await _currentUserService.GetUserInfo();
             var isUserInfoValid = _userDataValidationService.Check(auth0UserInfo);
 
             if (!isUserInfoValid)
             {
-                throw new CurrentUserDataCheckValidationException("CurrentAuth0UserInfo in UserSettingsAction didn't pass validation");
+                _logger.LogError("Auth0UserInfo in GetAuth0UsersByCurrentUserAction didn't pass validation as it has nulls");
+                throw new InvalidAuth0DataException();
             }
 
             var currentUserAuth0UsersList = await _userSettingsService.GetAllAuth0UsersFromUser(auth0UserInfo);
