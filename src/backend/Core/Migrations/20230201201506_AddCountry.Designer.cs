dbc48eae3afe2284c5c278fb717a406197411065
h4570 <sandro.sobczynski@gmail.com>
2023-02-01 21:18:59 +0100

    Add countries table

--- src/backend/Core/Migrations/20230201201506_AddCountry.Designer.cs
+++ src/backend/Core/Migrations/20230201201506_AddCountry.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Core.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230201201506_AddCountry")]
+    partial class AddCountry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/Core/Migrations/20230201201506_AddCountry.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "countries",
+                schema: "core",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    englishname = table.Column<string>(name: "english_name", type: "character varying(50)", maxLength: 50, nullable: false),
+                    alpha2isocode = table.Column<string>(name: "alpha2iso_code", type: "character varying(2)", maxLength: 2, nullable: false),
+                    alpha3isocode = table.Column<string>(name: "alpha3iso_code", type: "character varying(3)", maxLength: 3, nullable: false),
+                    updateuserid = table.Column<long>(name: "update_user_id", type: "bigint", nullable: false),
+                    updatedat = table.Column<DateTime>(name: "updated_at", type: "timestamp with time zone", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_countries", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_countries_users_update_user_id",
+                        column: x => x.updateuserid,
+                        principalSchema: "core",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_countries_alpha2iso_code",
+                schema: "core",
+                table: "countries",
+                column: "alpha2iso_code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_countries_alpha3iso_code",
+                schema: "core",
+                table: "countries",
+                column: "alpha3iso_code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_countries_update_user_id",
+                schema: "core",
+                table: "countries",
+                column: "update_user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "countries",
+                schema: "core");
+        }
+    }
+}
