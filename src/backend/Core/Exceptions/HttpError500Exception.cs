5bd2a2a4fa4916710bc6e2625ecadddb3ea82ef4
s21166-pj <s21166@pjwstk.edu.pl>
2023-02-01 11:59:33 +0100

    base implementation for SerializableException pattern

--- src/backend/Core/Exceptions/HttpError500Exception.cs
+++ src/backend/Core/Exceptions/HttpError500Exception.cs
@@ -5,8 +5,22 @@ namespace Core.Exceptions
     [Serializable]
     public class HttpError500Exception : Exception
     {
+        public HttpError500Exception()
+        {
+        }
+
         public HttpError500Exception(string message)
             : base($"{message}. Please, contact system administrator")
+        {
+        }
+
+        public HttpError500Exception(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public HttpError500Exception(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
