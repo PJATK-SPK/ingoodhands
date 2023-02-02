5c80c9be36d49e14153fd86c1e5af1db565ec72a
h4570 <sandro.sobczynski@gmail.com>
2023-02-02 16:02:13 +0100

    Fix exceptions styling

--- src/backend/Core/Exceptions/HttpError500Exception.cs
+++ src/backend/Core/Exceptions/HttpError500Exception.cs
@@ -5,14 +5,8 @@ namespace Core.Exceptions
     [Serializable]
     public class HttpError500Exception : Exception
     {
-        public HttpError500Exception(string message)
-            : base($"{message}. Please, contact system administrator")
-        {
-        }
+        public HttpError500Exception(string message) : base($"{message}. Please, contact system administrator") { }
 
-        protected HttpError500Exception(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
+        protected HttpError500Exception(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
