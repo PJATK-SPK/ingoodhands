3f8bea0302f9fb59c0d49c0fa1e490b3547a92aa
marcel <marcel.pankanin@ramp.network>
2023-02-03 20:02:46 +0100

    feat(infra): fix sonarcoverage

---
+++ src/backend/Core/Migrations/20221124212836.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+         Var user = new User();
+         user = null;
+    }
+}
