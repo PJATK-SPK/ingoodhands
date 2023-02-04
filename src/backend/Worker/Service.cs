6d417813e08f22fb45edb58a380f77e05024691b
h4570 <sandro.sobczynski@gmail.com>
2023-02-04 20:50:20 +0100

    fix service

--- src/backend/Worker/Service.cs
+++ src/backend/Worker/Service.cs
@@ -1,23 +1,25 @@
 using Core.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace Worker
 {
     public class Service : BackgroundService
     {
         private readonly ILogger<Service> _logger;
+        private readonly AppDbContext _appDbContext;
 
-        public Service(ILogger<Service> logger, AppDbContext dbContext)
+        public Service(ILogger<Service> logger, AppDbContext appDbContext)
         {
             _logger = logger;
-
-            var test = dbContext.Users.ToList();
+            _appDbContext = appDbContext;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Database users count: {count}", _appDbContext.Users.Count());
+
                 await Task.Delay(1000, stoppingToken);
             }
         }
