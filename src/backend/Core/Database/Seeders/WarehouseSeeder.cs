af7d6831a0803081ab171ff178c668e597028c84
marcel <marcel.pankanin@ramp.network>
2023-02-08 19:55:06 +0100

    feat(backend): add warehouse seeder

---
+++ src/backend/Core/Database/Seeders/WarehouseSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Core.Database.Enums;
+using Core.Database.Models.Auth;
+using Core.Database.Models.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Database.Seeders
+{
+    public static class WarehouseSeeder
+    {
+        public static void Execute(ModelBuilder builder)
+        {
+            builder.Entity<Warehouse>().HasData(new Warehouse()
+            {
+                Id = 1,
+                Status = DbEntityStatus.Active,
+                ShortName = "PLN001",
+                UpdatedAt = DateTime.UtcNow,
+                UpdateUserId = 1,
+                Address = new Address()
+                {
+                    Id = 1,
+                    Status = DbEntityStatus.Active,
+                    UpdatedAt = DateTime.UtcNow,
+                    UpdateUserId = 1,
+                }
+           
+            });
+        }
+    }
+}
