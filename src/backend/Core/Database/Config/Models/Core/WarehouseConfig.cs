a7407623a44350e7871c8f1e9e00e481a03a420e
marcel <marcel.pankanin@ramp.network>
2023-02-08 19:45:00 +0100

    feat(backend): add warehouse table

---
+++ src/backend/Core/Database/Config/Models/Core/WarehouseConfig.cs
@@ -0,0 +1,21 @@
+﻿using Core.Database.Config.Base;
+using Core.Database.Extensions;
+using Core.Database.Models.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Core.Database.Config.Models.Core
+{
+    public class WarehouseConfig<TBase> : IEntityTypeConfiguration<TBase>
+    where TBase : Warehouse
+    {
+        public virtual void Configure(EntityTypeBuilder<TBase> builder)
+        {
+            new DbEntityConfig<TBase>().Configure(builder);
+            builder.ToTable("warehouses", "core");
+
+            builder.Property(c => c.ShortName).IsRequired().HasMaxLength(5);
+            builder.HasOne(c => c.Address).WithMany(c => (IEnumerable<TBase>?)c.Warehouses).HasForeignKey(c => c.AddressId).HasConstraintName("warehouses_address_id_fkey");
+        }
+    }
+}
