dc86cf17792d57a5b7dc3442d33216081fda240a
h4570 <sandro.sobczynski@gmail.com>
2023-02-09 19:57:30 +0100

    fix(seeder): add migration

--- src/backend/Core/Database/Seeders/WarehouseSeeder.cs
+++ src/backend/Core/Database/Seeders/WarehouseSeeder.cs
@@ -1,5 +1,4 @@
 ﻿using Core.Database.Enums;
-using Core.Database.Models.Auth;
 using Core.Database.Models.Core;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,200 +12,82 @@ namespace Core.Database.Seeders
             {
                 Id = 1,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "PL001",
-                Address = new Address()
-                {
-                    Id = 1,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 177,
-                    PostalCode = "80-503",
-                    City = "Gdansk",
-                    Street = "al. gen. J. Hallera",
-                    StreetNumber = "239",
-                    GpsLatitude = 54.40672743695016, 
-                    GpsLongitude = 18.626590482862696
-                }
+                AddressId = AddressSeeder.Address1Poland.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 2,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "PL002",
-                Address = new Address()
-                {
-                    Id = 2,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 177, 
-                    PostalCode = "30-701",
-                    City = "Krakow",
-                    Street = "ul. Zablocie",
-                    StreetNumber = "20",
-                    Apartment = "22",
-                    GpsLatitude = 50.050510416252955,
-                    GpsLongitude = 19.96738355797516
-                }
+                AddressId = AddressSeeder.Address2Poland.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 3,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "PL003",
-                Address = new Address()
-                {
-                    Id = 3,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 177,
-                    PostalCode = "05-820",
-                    City = "Piastow",
-                    Street = "ul. St. Bodycha",
-                    StreetNumber = "97",
-                    GpsLatitude = 52.180876508945325,
-                    GpsLongitude = 20.864602669100716
-                }
+                AddressId = AddressSeeder.Address3Poland.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 4,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "DE001",
-                Address = new Address()
-                {
-                    Id = 4,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 84,
-                    PostalCode = "13435",
-                    City = "Berlin",
-                    Street = "Wallenroder Strasse",
-                    StreetNumber = "7",
-                    GpsLatitude = 52.6243741245117,  
-                    GpsLongitude = 13.337297904503853
-                }
+                AddressId = AddressSeeder.Address4Germany.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 5,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "DE002",
-                Address = new Address()
-                {
-                    Id = 5,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 84,
-                    PostalCode = "81677",
-                    City = "Munich",
-                    Street = "Kronstadter Str", 
-                    StreetNumber = "30",
-                    GpsLatitude = 48.141578533011334,  
-                    GpsLongitude = 11.648666754927092
-                }
+                AddressId = AddressSeeder.Address5Germany.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 6,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "HU001",
-                Address = new Address()
-                {
-                    Id = 6,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 101,
-                    PostalCode = "1097",
-                    City = "Budapest", 
-                    Street = "Feherakac u.",
-                    StreetNumber = "821",
-                    GpsLatitude = 47.469941092986005,
-                    GpsLongitude = 19.109420384400256
-                }
+                AddressId = AddressSeeder.Address6Hungary.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 7,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "CZ001",
-                Address = new Address()
-                {
-                    Id = 7,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 61,
-                    PostalCode = "17000",
-                    City = "Prague",
-                    Street = "Argentinska",
-                    StreetNumber = "516",
-                    Apartment = "40",
-                    GpsLatitude = 50.12465893170012,
-                    GpsLongitude = 14.439680032281526
-                }
+                AddressId = AddressSeeder.Address7Czech.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 8,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "FR001",
-                Address = new Address()
-                {
-                    Id = 8,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 77,
-                    PostalCode = "06200",
-                    City = "Nice",
-                    Street = "Pass. du Fret",
-                    GpsLatitude = 43.66614093758796,
-                    GpsLongitude = 7.202632419942202
-                }
+                AddressId = AddressSeeder.Address8France.Id
             });
             builder.Entity<Warehouse>().HasData(new Warehouse()
             {
                 Id = 9,
                 Status = DbEntityStatus.Active,
-                UpdatedAt = new DateTime(2023, 01, 01),
+                UpdatedAt = new DateTime(2023, 01, 01, 0, 0, 0, DateTimeKind.Utc),
                 UpdateUserId = 1,
                 ShortName = "FR002",
-                Address = new Address()
-                {
-                    Id = 9,
-                    Status = DbEntityStatus.Active,
-                    UpdatedAt = new DateTime(2023, 01, 01),
-                    UpdateUserId = 1,
-                    CountryId = 77,
-                    PostalCode = "13435",
-                    City = "Berlin",
-                    Street = "Wallenroder Strasse",
-                    StreetNumber = "7",
-                    GpsLatitude = 52.6243741245117,  
-                    GpsLongitude = 13.337297904503853
-                }
+                AddressId = AddressSeeder.Address9France.Id
             });
         }
     }
