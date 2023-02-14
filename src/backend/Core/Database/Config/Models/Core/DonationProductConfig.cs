9ddffc574d60368eddb9e9e4ae5e471e0f54c776
marcel <marcel.pankanin@ramp.network>
2023-02-14 01:21:17 +0100

    initial donation table

--- src/backend/Core/Database/Config/Models/Core/DonationProductConfig.cs
+++ src/backend/Core/Database/Config/Models/Core/DonationProductConfig.cs
@@ -13,7 +13,7 @@ namespace Core.Database.Config.Models.Core
         public virtual void Configure(EntityTypeBuilder<TBase> builder)
         {
             new DbEntityConfig<TBase>().Configure(builder);
-            builder.ToTable("donations", "core");
+            builder.ToTable("donation_products", "core");
             builder.Property(c => c.Quantity).IsRequired();
 
             builder.HasOne(c => c.Donation).WithMany(c => (IEnumerable<TBase>?)c.Products).HasForeignKey(c => c.DonationId).HasConstraintName("donations_products_id_fkey");
