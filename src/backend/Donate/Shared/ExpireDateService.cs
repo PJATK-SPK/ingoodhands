a39c8d342d3576649f05d9c8a0e5bb8453ef5c0d
h4570 <sandro.sobczynski@gmail.com>
2023-02-18 17:12:18 +0100

    fix(sonar): fixes

--- src/backend/Donate/Shared/ExpireDateService.cs
+++ src/backend/Donate/Shared/ExpireDateService.cs
@@ -4,7 +4,7 @@ namespace Donate.Shared
 {
     public class ExpireDateService
     {
-        public readonly int ExpireDays = 30;
+        public static readonly int ExpireDays = 30;
 
         public static DateTime GetExpiredDate4Today() => DateTime.UtcNow.Date.AddDays(-30);
         public static DateTime GetExpiredDate4Donation(Donation donation) => GetExpiredDate4Donation(donation.CreationDate);
