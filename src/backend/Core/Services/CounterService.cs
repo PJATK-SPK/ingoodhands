44ab13f7759bb3258b5a868474476f7fb584e9a1
s21166-pj <s21166@pjwstk.edu.pl>
2023-02-25 13:42:41 +0100

    created counterService for getting counter and updating it

---
+++ src/backend/Core/Services/CounterService.cs
@@ -0,0 +1,46 @@
+﻿using Core.Database;
+using Core.Database.Models.Core;
+using Core.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class CounterService
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ILogger<CounterService> _logger;
+
+        public CounterService(AppDbContext appDbContext, ILogger<CounterService> logger)
+        {
+            _appDbContext = appDbContext;
+            _logger = logger;
+        }
+
+        public async Task<Counters> GetCounter(string tableName)
+        {
+            var counterOfSpecifiedTable = await _appDbContext.Counters.SingleOrDefaultAsync(c => c.Name == tableName);
+            if (counterOfSpecifiedTable == null)
+            {
+                _logger.LogError("Cannot find specified tableName in Counters table");
+                throw new ApplicationErrorException("Sorry there seems to be problem with our service");
+            }
+            return counterOfSpecifiedTable;
+        }
+
+        public long GetAndUpdateNextCounter(string tableName)
+        {
+            var nextCounterOfSpecifiedTable = GetCounter(tableName);
+            nextCounterOfSpecifiedTable.Result.Value++;
+
+            _appDbContext.SaveChanges();
+
+            return nextCounterOfSpecifiedTable.Result.Value;
+        }
+    }
+}
