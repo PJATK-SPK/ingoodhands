9cd0e9a4c6cfd168b19a99ef36102c98a0521b81
s21166-pj <s21166@pjwstk.edu.pl>
2023-03-01 19:22:56 +0100

    Fixed bug with showing all notDeliveredDonations

--- src/backend/Donate/Actions/MyDonations/GetNotDeliveredCount/GetNotDeliveredCountAction.cs
+++ src/backend/Donate/Actions/MyDonations/GetNotDeliveredCount/GetNotDeliveredCountAction.cs
@@ -1,4 +1,6 @@
 ﻿using Core.Database;
+using Core.Services;
+using Core.Setup.Auth0;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -12,16 +14,23 @@ namespace Donate.Actions.MyDonations.GetNotDeliveredCount
     public class GetNotDeliveredCountAction
     {
         private readonly AppDbContext _appDbContext;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly GetCurrentUserService _getCurrentUserService;
 
-        public GetNotDeliveredCountAction(AppDbContext appDbContext)
+        public GetNotDeliveredCountAction(AppDbContext appDbContext, ICurrentUserService currentUserService, GetCurrentUserService getCurrentUserService)
         {
             _appDbContext = appDbContext;
+            _currentUserService = currentUserService;
+            _getCurrentUserService = getCurrentUserService;
         }
 
         public async Task<OkObjectResult> Execute()
         {
+            var auth0UserInfo = await _currentUserService.GetUserInfo();
+            var currentUser = await _getCurrentUserService.Execute(auth0UserInfo);
+
             var numberOfNotDeliveredDonations = await _appDbContext.Donations
-                .Where(c => !c.IsDelivered && !c.IsExpired)
+                .Where(c => c.CreationUserId == currentUser.Id && !c.IsDelivered && !c.IsExpired)
                 .CountAsync();
 
             return new OkObjectResult(new GetNotDeliveredCountResponse { Count = numberOfNotDeliveredDonations });
