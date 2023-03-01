3f364f0b71a8b1f6bd2d91b86c405b27540257f6
h4570 <sandro.sobczynski@gmail.com>
2023-03-01 21:50:07 +0100

    feat(manage-users): add list

--- src/frontend/src/app/pages/secure/manage-users/manage-users.component.ts
+++ src/frontend/src/app/pages/secure/manage-users/manage-users.component.ts
@@ -1,31 +1,51 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { ManageUsersService } from './services/manage-users.service';
+import { ManageUsersItem } from './models/manage-users-item';
+import { PagedResult } from 'src/app/interfaces/paged-result';
+import { Role } from 'src/app/enums/role';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
+import { Destroy } from 'src/app/services/destroy';
 
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
   styleUrls: ['./manage-users.component.scss'],
   providers: [
+    ManageUsersService,
+    Destroy,
   ]
 })
 export class ManageUsersComponent implements OnInit {
+  public Role = Role;
+  public page = 1;
+  public pageSize = 10;
+  public onFilterChange = new Subject<Event>();
+  public filter: string | undefined;
+  public pagedResult: PagedResult<ManageUsersItem> | undefined;
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(
+    private readonly service: ManageUsersService,
+    private readonly destroy$: Destroy) { }
 
   public ngOnInit(): void {
-    this.getUsers(1, 10, 'test').subscribe();
+    this.fetch();
+
+    this.onFilterChange
+      .pipe(
+        takeUntil(this.destroy$),
+        debounceTime(700),
+        distinctUntilChanged()
+      ).subscribe(() => this.fetch());
   }
 
-  // to service
-  public getUsers(page: number, pageSize: number, filter: string): Observable<any> {
-    return this.http.get<any>(environment.api + '/manage-users', {
-      params: {
-        page: page.toString(),
-        pageSize: pageSize.toString(),
-        filter
-      }
+  public paginate(event: { first: number, page: number, pageCount: number, rows: number }): void {
+    this.page = event.page + 1;
+    this.fetch();
+  }
+
+  private fetch(): void {
+    this.service.getUsers(this.page, this.pageSize, this.filter).subscribe(result => {
+      this.pagedResult = result;
     });
   }
 
--- src/frontend/src/app/pages/secure/manage-users/manage-users.module.ts
+++ src/frontend/src/app/pages/secure/manage-users/manage-users.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageUsersComponent } from './manage-users.component';
 import { CardModule } from 'primeng/card';
+import { RouterModule } from '@angular/router';
+import { TagModule } from 'primeng/tag';
+import { PaginatorModule } from 'primeng/paginator';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @NgModule({
   declarations: [
@@ -10,6 +16,12 @@ import { CardModule } from 'primeng/card';
   imports: [
     CommonModule,
     CardModule,
+    RouterModule,
+    TagModule,
+    PaginatorModule,
+    InputTextModule,
+    FormsModule,
+    StyleClassModule,
   ],
   exports: [
     ManageUsersComponent
