a4583a26f4be8c699dc6f974bfbdc2667028b4d4
s21166-pj <s21166@pjwstk.edu.pl>
2023-03-09 21:13:01 +0100

    update indexes on Order and Delivery table, added migration

---
+++ src/backend/Core/Migrations/20230309201158_UpdateIndexes.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "percentage_idx",
+                schema: "core",
+                table: "orders");
+
+            migrationBuilder.CreateIndex(
+                name: "name_idx",
+                schema: "core",
+                table: "orders",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "percentage_idx",
+                schema: "core",
+                table: "orders",
+                column: "percentage");
+
+            migrationBuilder.CreateIndex(
+                name: "name_idx",
+                schema: "core",
+                table: "deliveries",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "name_idx",
+                schema: "core",
+                table: "orders");
+
+            migrationBuilder.DropIndex(
+                name: "percentage_idx",
+                schema: "core",
+                table: "orders");
+
+            migrationBuilder.DropIndex(
+                name: "name_idx",
+                schema: "core",
+                table: "deliveries");
+
+            migrationBuilder.CreateIndex(
+                name: "percentage_idx",
+                schema: "core",
+                table: "orders",
+                column: "percentage",
+                unique: true);
+        }
+    }
+}
