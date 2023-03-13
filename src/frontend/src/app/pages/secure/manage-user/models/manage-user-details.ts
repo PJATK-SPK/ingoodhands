3ab2e58c44c042926b6907e5130bcfd574d2e419
h4570 <sandro.sobczynski@gmail.com>
2023-03-13 21:05:56 +0100

    add email

--- src/frontend/src/app/pages/secure/manage-user/models/manage-user-details.ts
+++ src/frontend/src/app/pages/secure/manage-user/models/manage-user-details.ts
@@ -3,6 +3,7 @@ import { Role } from "src/app/enums/role";
 export interface ManageUserDetails {
     id: string;
     fullName: string;
+    email: string;
     warehouseId: string;
     roles: Role[];
 }
