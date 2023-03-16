1610c33523a8b15ef9621ad8a7deafb1d1591a1a
s21166-pj <s21166@pjwstk.edu.pl>
2023-03-16 15:46:52 +0100

    create Orders Endpoints

---
+++ src/backend/Order/Actions/OrdersActions/OrdersGetSingle/OrdersGetSingleDeliveryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders.Actions.OrdersActions.OrdersGetSingle
+{
+    public class OrdersGetSingleDeliveryResponse
+    {
+        public string Name { get; set; } = default!;
+        public DateTime CreationDate { get; set; } = default!;
+        public bool IsDelivered { get; set; } = default!;
+    }
+}
---
+++ src/backend/Order/Actions/OrdersActions/OrdersGetSingle/OrdersGetSingleModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Core.Setup.Autofac;
+
+namespace Orders.Actions.OrdersActions.OrdersGetSingle
+{
+    public class OrdersGetSingleModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAsScoped<OrdersGetSingleAction>();
+            builder.RegisterAsScoped<OrdersGetSingleService>();
+        }
+    }
+}
