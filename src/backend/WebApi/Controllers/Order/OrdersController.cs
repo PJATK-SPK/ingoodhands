8ce0a90d1c2ad5767d0fab96c0f1c189d9aea9bd
s21166-pj <s21166@pjwstk.edu.pl>
2023-03-16 16:00:58 +0100

    remove commented code

--- src/backend/WebApi/Controllers/Order/OrdersController.cs
+++ src/backend/WebApi/Controllers/Order/OrdersController.cs
@@ -18,56 +18,6 @@ public class OrdersController : ControllerBase
 
     [HttpGet("{id}")]
     public async Task<ActionResult> GetSingle(string id) => await _ordersGetSingleAction.Execute(id);
-    //{
-    //    var appDbContextProducts = new List<DeleteMeOrderResponseItem>
-    //    {
-    //       new DeleteMeOrderResponseItem
-    //       {
-    //           Id = "knb34",
-    //           Name = "ORD000001",
-    //           Percentage = 25,
-    //           CreationDate = DateTime.UtcNow.AddDays(-5),
-    //           CountryName = "Ukraine",
-    //           GpsLatitude = 12.23,
-    //           GpsLongitude = 23.34,
-    //           City="Kyiv",
-    //           PostalCode="00-000",
-    //           Street = "Gdañska 1",
-    //           Deliveries = new List<DeleteMeOrderDeliveryResponse>
-    //           {
-    //                new DeleteMeOrderDeliveryResponse
-    //                {
-    //                     Name = "DEL000002",
-    //                     CreationDate= DateTime.UtcNow.AddDays(-5),
-    //                     IsDelivered=false,
-    //                },
-    //                new DeleteMeOrderDeliveryResponse
-    //                {
-    //                     Name = "DEL000001",
-    //                     CreationDate= DateTime.UtcNow.AddDays(-10),
-    //                     IsDelivered=true,
-    //                }
-    //           },
-    //           Products= new List<DeleteMeOrderProductResponse>
-    //           {
-    //               new DeleteMeOrderProductResponse
-    //               {
-    //                    Name="Milk",
-    //                    Quantity=10,
-    //                    Unit = "l"
-    //               },
-    //               new DeleteMeOrderProductResponse
-    //               {
-    //                    Name="Rice",
-    //                    Quantity=15,
-    //                    Unit = "kg"
-    //               }
-    //           }
-    //       }
-    //    };
-
-    //    return await Task.FromResult(Ok(appDbContextProducts));
-    //}
 
     [HttpPost("{id}/cancel")]
     public async Task<ActionResult> Cancel(string id)
