ccdaf19c457ee8e947a05e788bbb574a369c0ea7
s21166-pj <s21166@pjwstk.edu.pl>
2023-04-21 13:01:59 +0200

    make JobService static

--- src/backend/Orders/Jobs/RecalcOrdersPercentage/RecalcOrdersPercentageJob.cs
+++ src/backend/Orders/Jobs/RecalcOrdersPercentage/RecalcOrdersPercentageJob.cs
@@ -7,15 +7,13 @@ namespace Orders.Jobs.RecalcOrdersPercentage
     {
         private readonly AppDbContext _appDbContext;
         private readonly RecalcOrdersPercentageJobDataService _recalcOrdersPercentageJobDataService;
-        private readonly RecalcOrdersPercentageJobService _recalcOrdersPercentageJobService;
+
 
         public RecalcOrdersPercentageJob(
             RecalcOrdersPercentageJobDataService recalcOrdersPercentageJobDataService,
-            RecalcOrdersPercentageJobService recalcOrdersPercentageJobService,
             AppDbContext appDbContext)
         {
             _recalcOrdersPercentageJobDataService = recalcOrdersPercentageJobDataService;
-            _recalcOrdersPercentageJobService = recalcOrdersPercentageJobService;
             _appDbContext = appDbContext;
         }
 
@@ -26,7 +24,7 @@ namespace Orders.Jobs.RecalcOrdersPercentage
 
             foreach (var order in listOfOrders)
             {
-                _recalcOrdersPercentageJobService.CalculateAndUpdatePercentage(order);
+                RecalcOrdersPercentageJobService.CalculateAndUpdatePercentage(order);
             }
 
             await _appDbContext.SaveChangesAsync();
--- src/backend/Orders/Jobs/RecalcOrdersPercentage/RecalcOrdersPercentageJobService.cs
+++ src/backend/Orders/Jobs/RecalcOrdersPercentage/RecalcOrdersPercentageJobService.cs
@@ -5,7 +5,7 @@ namespace Orders.Jobs.RecalcOrdersPercentage
 {
     public class RecalcOrdersPercentageJobService
     {
-        public int CalculateAndUpdatePercentage(Order order)
+        public static int CalculateAndUpdatePercentage(Order order)
         {
             int totalQuantity = order.OrderProducts!.Sum(op => op.Quantity);
             int deliveredQuantity = order.Deliveries!.Where(del => del.IsDelivered).Sum(d => d.DeliveryProducts!.Sum(dp => dp.Quantity));
