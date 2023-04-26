30e208cb12941ee6bb1441e96e15546988b4389f
s21166-pj <s21166@pjwstk.edu.pl>
2023-04-26 08:51:19 +0200

    added roleChecker to action

--- src/backend/Orders/Actions/DeliveriesActions/DeliveriesPickup/DeliveriesPickupAction.cs
+++ src/backend/Orders/Actions/DeliveriesActions/DeliveriesPickup/DeliveriesPickupAction.cs
@@ -6,23 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Orders.Actions.DeliveriesActions.DeliveriesGetSingle;
+using Core.Services;
 
 namespace Orders.Actions.DeliveriesActions.DeliveriesPickup
 {
     public class DeliveriesPickupAction
     {
         private readonly AppDbContext _appDbContext;
+        private readonly RoleService _roleService;
         private readonly Hashids _hashids;
         private readonly ILogger<DeliveriesPickupAction> _logger;
 
-        public DeliveriesPickupAction(AppDbContext appDbContext, Hashids hashids, ILogger<DeliveriesPickupAction> logger)
+        public DeliveriesPickupAction(AppDbContext appDbContext, Hashids hashids, ILogger<DeliveriesPickupAction> logger, RoleService roleService)
         {
             _appDbContext = appDbContext;
             _hashids = hashids;
             _logger = logger;
+            _roleService = roleService;
         }
         public async Task<OkObjectResult> Execute(string id)
         {
+            await _roleService.ThrowIfNoRole(RoleName.WarehouseKeeper);
+
             var decodedDeliveryId = _hashids.DecodeSingleLong(id);
             var dbResult = await _appDbContext.Deliveries.SingleOrDefaultAsync(c => c.Id == decodedDeliveryId && c.Status == DbEntityStatus.Active);
 
