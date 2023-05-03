bf208b27c8c594392eafb923e3d326bd3894465d
s21166-pj <s21166@pjwstk.edu.pl>
2023-05-03 21:48:36 +0200

    git add count endpoint for availableControler + IT

---
+++ src/backend/Orders/Actions/AvailableDeliveriesActions/AvailableDeliveriesCount/AvailableDeliveriesCountModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Core.Setup.Autofac;
+
+namespace Orders.Actions.AvailableDeliveriesActions.AvailableDeliveriesCount
+{
+    public class AvailableDeliveriesCountModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAsScoped<AvailableDeliveriesCountAction>();
+            builder.RegisterAsScoped<AvailableDeliveriesCountService>();
+        }
+    }
+}
